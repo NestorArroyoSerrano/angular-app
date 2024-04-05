40023f95c1abeb7c5f1e0553eb3eb0e58ededdba
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-05 10:52:28 +0200

    Función submit

--- src/app/products/components/product.component.ts
+++ src/app/products/components/product.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
+import { FormComponent } from './form.component';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -13,8 +14,14 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
 
   constructor(private service: ProductService) { }
-  
+
   ngOnInit(): void {
     this.service.findAll().subscribe(products => this.products = products);
+  }
+
+  addProduct(product: Product){
+    product.id = new Date().getTime();
+    this.products.push(product);
+   // this.products = [... this.products, {...product, id: new Date().getTime()}];
   }
 }
