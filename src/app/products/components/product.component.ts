472df7da511f3f073d73022bb650b1a9b5146af4
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-05 11:34:38 +0200

    CRUD acabado

--- src/app/products/components/product.component.ts
+++ src/app/products/components/product.component.ts
@@ -13,15 +13,37 @@ import { FormComponent } from './form.component';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
 
+  productSelected: Product = new Product();
+
   constructor(private service: ProductService) { }
 
   ngOnInit(): void {
     this.service.findAll().subscribe(products => this.products = products);
   }
 
-  addProduct(product: Product){
-    product.id = new Date().getTime();
-    this.products.push(product);
-   // this.products = [... this.products, {...product, id: new Date().getTime()}];
+  addProduct(product: Product): void{
+    if(product.id>0){
+      this.products = this.products.map(prod => {
+        if(prod.id== product.id){
+          return {...product}
+        }
+        return prod;
+      })
+
+    }else {
+     /*  product.id = new Date().getTime();
+      this.products.push(product); */
+
+      this.products = [... this.products, {...product, id: new Date().getTime()}];
+    }
+    this.productSelected = new Product();
   }
+  onRemoveProduct(id: number): void {
+    this.products = this.products.filter(product => product.id != id);
+  }
+
+  onUpdateProduct(productRow: Product): void {
+    this.productSelected = productRow;
+  }
+
 }
