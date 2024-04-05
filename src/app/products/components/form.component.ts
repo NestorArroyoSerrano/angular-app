7002c7670b65a5c7992fdcf712078577fadd8d59
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-05 13:25:40 +0200

    Validación del formulario

--- src/app/products/components/form.component.ts
+++ src/app/products/components/form.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../models/product';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'product-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -24,5 +25,14 @@ export class FormComponent {
     this.newProductEvent.emit(this.product);
     console.log(this.product)
   }
-}
 
+  clean(): void {
+    //this.product = new Product();
+    this.product = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0
+  };
+}
+}
